910bc3f1cc8b49f602429ecf6fb43d197643974d
amir-karimi96 <amir.karimi96@gmail.com>
2021-02-04 19:28:19 -0700

    complete fetch scripts

--- deep_rl/scripted_agents/scripted.py
+++ deep_rl/scripted_agents/scripted.py
@@ -1,7 +1,43 @@
 import numpy as np
+import torch
+class fetch_reach_object():
+    def __init__(self):
+        pass
+    def step(self,obs):
+        #print(obs)
+        action = np.ones((obs.shape[0],4),dtype=np.float32)
+        action[:,:3] = (obs[:,3:6]-obs[:,6:9]-torch.tensor([0,0,-0.05]))*5
+        return action
 
-def fetch_reacher(obs):
-    print(obs)
-    action = np.zeros((obs.shape[0],4),dtype=np.float32)
-    action[:,:3] = (obs[:,:3]-obs[:,6:9])*1
-    return action
+    def beta(self,obs):
+        d=np.linalg.norm(obs[:,3:6]-obs[:,6:9]-torch.tensor([0,0,-0.0]))
+        return np.ones((obs.shape[0],1))*(d<0.06)#(1-np.exp(-d*20))
+
+class fetch_reach_goal():
+    def __init__(self):
+        pass
+    def step(self,obs):
+        #print(obs)
+        action = np.zeros((obs.shape[0],4),dtype=np.float32)-1
+        action[:,:3] = (obs[:,:3]-obs[:,6:9])*4
+        return action
+
+    def beta(self,obs):
+        d=np.linalg.norm(obs[:,3:6]-obs[:,6:9])
+        return np.ones((obs.shape[0],1))*(d>0.05)#*(np.exp(-d*20))
+
+class fetch_grasp():
+    def __init__(self):
+        pass
+    def step(self,obs):
+        #print(obs)
+        action = np.zeros((obs.shape[0],4),dtype=np.float32)-1
+        action[:,:3] = (obs[:,3:6]-obs[:,6:9])*4
+        action[:,3] = (np.linalg.norm(obs[:,3:6]-obs[:,6:9])-0.05)
+        return action
+
+    def beta(self,obs):
+        d=np.linalg.norm(obs[:,3:6]-obs[:,6:9])
+        d1=np.linalg.norm(obs[:,3:6]-obs[:,6:9])
+        # print(d)
+        return np.ones((obs.shape[0],1))*((d>0.07)+(d<0.02))#*(np.exp(-d*20))
